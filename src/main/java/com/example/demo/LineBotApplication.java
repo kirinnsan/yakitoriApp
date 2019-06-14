9ff26f7298d96ff27fa6a183427eec00032e236b
kirinnsan <hirosi.t822@gmail.com>
2019-06-15 06:11:39 +0900

    linebotライブラリ機能を追加

--- src/main/java/com/example/demo/LineBotApplication.java
+++ src/main/java/com/example/demo/LineBotApplication.java
@@ -3,11 +3,33 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.linecorp.bot.model.event.Event;
+import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.Message;
+import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.spring.boot.annotation.EventMapping;
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+
+@LineMessageHandler
 @SpringBootApplication
 public class LineBotApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LineBotApplication.class, args);
 	}
+
+    @EventMapping
+    public Message handleTextMessageEvent(MessageEvent<TextMessageContent> event) {
+        System.out.println("event: " + event);
+        // URLを返却
+        return  new TextMessage("https://yakitoriapp.herokuapp.com/");
+    }
+
+    @EventMapping
+    public void handleDefaultMessageEvent(Event event) {
+        System.out.println("event: " + event);
+
+    }
 
 }
